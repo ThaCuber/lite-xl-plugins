066eca5dc2c3d5aec637ae6fccf980c276fb5496
Luke aka SwissalpS <Luke@SwissalpS.ws>
2023-06-29 21:54:13 +0200

    respect user settings of disabled plugins
    if we don't check settings, we may be force-loading a plugin that user
    did not want active.

--- plugins/copyfilelocation.lua
+++ plugins/copyfilelocation.lua
@@ -1,9 +1,15 @@
 -- mod-version:3
 local core = require "core"
 local command = require "core.command"
-local treeview = require 'plugins.treeview'
-local treemenu = treeview.contextmenu
-local mainmenu = require 'plugins.contextmenu'
+local config = require "core.config"
+local treeview, treemenu, mainmenu
+if false ~= config.plugins.treeview then
+  treeview = require "plugins.treeview"
+end
+if false ~= config.plugins.contextmenu then
+  treemenu = treeview and treeview.contextmenu
+  mainmenu = require "plugins.contextmenu"
+end
 
 command.add("core.docview!", {
   ["copy-file-location:of-doc"] = function(view)
@@ -17,23 +23,29 @@ command.add("core.docview!", {
   end
 })
 
-command.add(function()
-  return treeview.hovered_item ~= nil, treeview.hovered_item
-end, {
-  ["copy-file-location:of-tree-view-item"] = function(item)
-    if not (item and item.abs_filename) then
-      core.error "Cannot copy location of item"
-      return
-    end
-    core.log("Copying \"%s\" to clipboard", item.abs_filename)
-    system.set_clipboard(item.abs_filename)
-  end
-})
+if treeview then
+  command.add(function()
+      return treeview.hovered_item ~= nil, treeview.hovered_item
+    end, {
+      ["copy-file-location:of-tree-view-item"] = function(item)
+        if not (item and item.abs_filename) then
+          core.error "Cannot copy location of item"
+          return
+        end
+        core.log("Copying \"%s\" to clipboard", item.abs_filename)
+        system.set_clipboard(item.abs_filename)
+      end
+  })
+end
 
-treemenu:register(nil, {
-  { text = "Copy File Location", command = "copy-file-location:of-tree-view-item" }
-})
+if treemenu then
+  treemenu:register(nil, {
+    { text = "Copy File Location", command = "copy-file-location:of-tree-view-item" }
+  })
+end
 
-mainmenu:register("core.docview!", {
-  { text = "Copy File Location", command = "copy-file-location:of-doc" }
-})
+if mainmenu then
+  mainmenu:register("core.docview!", {
+    { text = "Copy File Location", command = "copy-file-location:of-doc" }
+  })
+end
